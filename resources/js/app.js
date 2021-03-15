74c64a7fd46ecf09cb15e32c1ea85f0a4fb90afa
David <davidcurbelomartin@gmail.com>
2021-03-15 19:15:14 +0000

    Instalación Element+

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-require('./bootstrap');
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+import locale from 'element-plus/lib/locale/lang/es';
+import componenteEjemplo from "./components/componenteEjemplo";
+
+createApp({
+    components: {
+        componenteEjemplo,
+    }
+}).use(ElementPlus, {locale}).mount('#app');
---
+++ resources/js/components/componenteEjemplo.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "componenteEjemplo"
+}
+</script>
+
+<style scoped>
+
+</style>
